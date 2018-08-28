b80b4ab6b9534d6dba81363032f6a6e5b8363101
Vinay <vinayraghava1988@gmail.com>
2018-08-28 20:58:03 +0530

    added H1

---
+++ src/main/java/p1/H1.java
@@ -0,0 +1,18 @@
+package p1;
+public class H1 {
+
+public String helloStr(String str){
+
+return "Hello"+str;
+
+}
+
+public static void main (String[] args){
+
+H1 h1 = new H1();
+
+System.out.println(h1.helloStr(" KVJ"));
+
+}
+
+}
